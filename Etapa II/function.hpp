650fd3927a644cd420cc4579d1be35b1885fef95
DAN2407 <00042220@uca.edu.sv>
2023-11-10 21:56:58 -0600

    avances, casi se logra!

--- Etapa II/function.hpp
+++ Etapa II/function.hpp
@@ -78,69 +78,40 @@ void show_post_order(tree *T) {
     }
 }
 
+
+void priority_queue (tree* T, order o){
+    if(o == IN){
+        show_in_order(T);
+    }else if(o == PRE){
+        show_pre_order(T);
+    }else if(o == POST){
+        show_post_order(T);
+    }
+}
+
+void show_priority_queue(tree* T){
+    priority_queue(T, IN);
+    std::cout << std::endl;
+    priority_queue(T, PRE);
+    std::cout << std::endl;
+    priority_queue(T, POST);
+    std::cout << std::endl;
+}
+
+void create_priority_queue(char symbol[], double probabilities[]) {
+    // Crear nodos para cada símbolo del alfabeto latino junto con sus probabilidades
+    tree *nodes[26];
+    for (int i = 0; i < 26; i++) {
+        nodes[i] = create_node(symbol[i], probabilities[i]);
+    }
+
+    // Insertar los nodos en la cola de prioridad
+    for (int i = 0; i < 26; i++) {
+        insert(root, nodes[i]);
+    }
+}
+
 //MIN Heapisize
 //creando swap
-void swap(tree* T, tree* smallest){
-    tree* aux = new tree;
-    aux->symbol = T->symbol;
-    aux->probability = T->probability;
-    aux->left = T->left;
-    aux->right = T->right;
-    aux->parent = T->parent;
-
-    T->symbol = smallest->symbol;
-    T->probability = smallest->probability;
-    T->left = smallest->left;
-    T->right = smallest->right;
-    T->parent = smallest->parent;
-
-    smallest->symbol = aux->symbol;
-    smallest->probability = aux->probability;
-    smallest->left = aux->left;
-    smallest->right = aux->right;
-    smallest->parent = aux->parent;
-}
-
-void MinHeapify(tree* T){
-    tree* smallest = T;
-    if(T->left != NULL && T->left->probability < smallest->probability){
-        smallest->probability = T->left->probability;
-    }
-    if(T->right != NULL && T->right->probability < smallest->probability){
-        smallest->probability = T->right->probability;
-    }
-    if(smallest != T){
-        swap(T, smallest);
-        MinHeapify(smallest);
-    }
-}
-
-void allMinHeapify(tree* T){
-    if(T->left != NULL){
-        allMinHeapify(T->left);
-    }
-    if(T->right != NULL){
-        allMinHeapify(T->right);
-    }
-    MinHeapify(T);
-}
-
-void insertPriorityQueue(tree* T, tree* new_node){
-    if(is_empty(T)){
-        root = new_node;
-    }else{
-        insert(T, new_node);
-        allMinHeapify(root);
-    }
-}
-
-void showPriorityQueue(tree* T){
-    if(!is_empty(T)){
-        std::cout << T->symbol << " " << T->probability << std::endl;
-        showPriorityQueue(T->left);
-        showPriorityQueue(T->right);
-    }
-}
-
 
 #endif 
--- Etapa II/main.cpp
+++ Etapa II/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include "function.hpp"
 
 int main() {
@@ -7,20 +6,29 @@ int main() {
     double probabilities[] = {0.83, 0.22, 0.03, 0.16, 0.41, 0.80, 0.72, 0.78, 0.73, 0.38, 0.99, 0.97, 0.73, 0.75, 0.02, 0.29, 0.99, 0.68, 0.65, 0.85, 0.87, 0.11, 0.97, 0.35, 0.22, 0.07, 0.68};
 
      // Crear nodos para cada símbolo del alfabeto latino junto con sus probabilidades
-    tree *nodes[27];
-    for (int i = 0; i < 27; i++) {
+    tree *nodes[26];
+    for (int i = 0; i < 26; i++) {
         nodes[i] = create_node(symbol[i], probabilities[i]);
     }
 
     //mostrar  nodos
-    for (int i = 0; i < 27; i++) {
+    for (int i = 0; i < 26; i++) {
         std::cout << nodes[i]->symbol << " " << nodes[i]->probability << std::endl;
     }
 
-    // Crear una cola de prioridad (montículo de mínimo) que contenga los nodos
-    for (int i = 0; i < 27; i++) {
-        nodes[i] = insertPriorityQueue(symbol[i], probabilities[i]);
+    // Crear una cola de prioridad (montículo de mínimo)
+    create_priority_queue(symbol, probabilities);
+
+
+    // Insertar los nodos en la cola de prioridad
+    for (int i = 0; i < 26; i++) {
+        insert(root, nodes[i]);
     }
+
+    // Mostrar la cola de prioridad
+    show_priority_queue(root);
+    
+
 
 
 
